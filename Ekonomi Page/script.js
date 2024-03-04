b1602b4d014e18aaa4cfe1090beab1fecba89dc2
Olalaolele123 <attayafaid@gmail.com>
2024-03-04 20:34:11 +0700

    Menambah branch untuk sosial budaya

---
+++ Ekonomi Page/script.js
@@ -0,0 +1,6 @@
+let modal = document.querySelector('.modal');
+let buttonMap = document.querySelector('.circle');
+
+buttonMap.addEventListener('click', () => {
+      modal.classList.toggle('active-modal')
+});
